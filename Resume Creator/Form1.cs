dadccd45b0c61d80ba6e448226a1c41b274cc1f8
DarylMickoSevilla <mickosevilla@gmail.com>
2022-07-11 23:30:57 +0800

    JSON File code reader

--- Resume Creator/Form1.cs
+++ Resume Creator/Form1.cs
@@ -11,7 +11,26 @@ namespace Resume_Creator
         {
             InitializeComponent();
         }
-
+        class Information
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Contact { get; set; }
+            public string Objective { get; set; }
+            public string Address { get; set; }
+            public string Elementary { get; set; }
+            public string JuniorHighSchool { get; set; }
+            public string SeniorHighSchool { get; set; }
+            public string College { get; set; }
+            public string ElementaryA { get; set; }
+            public string JuniorHighSchoolA { get; set; }
+            public string Hobbies { get; set; }
+            public override string ToString()
+            {
+                return string.Format("\nName: {0} \nEmail: {1} \nContact: {2} \nAddress: {4} \n\n {3} \n\nEducational Attainment \nElementary: {5} \nHigh School: {6} \nSenior High School: {7} \nCollege: {8} \n\nEducational Achievements \nElementary Achievement: {9} \nJunior HighSchool: {10} \n\nSkills: {11}",
+                        Name, Email, Contact, Objective, Address, Elementary, JuniorHighSchool, SeniorHighSchool, College, ElementaryA, JuniorHighSchoolA, string.Join("", Hobbies.ToArray()));
+            }
+        }
         private void Savebtn_Click(object sender, EventArgs e)
         {
 
@@ -27,5 +46,6 @@ namespace Resume_Creator
                 Information resultResume = JsonConvert.DeserializeObject<Information>(json);
                 InfoBox.Text = resultResume.ToString();
             }
+        }
     }
 }
